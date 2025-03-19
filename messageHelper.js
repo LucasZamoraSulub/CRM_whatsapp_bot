7d0ac1ae8ccb3764086dd1503778747301f05ce8
Lucas Eduardo Zamora Sulub <lucasosulub2@gmail.com>
2025-03-19 11:58:45 -0500

    COMMIT_Configuracion de CRM

--- messageHelper.js
+++ messageHelper.js
@@ -27,7 +27,59 @@ function getTextMessageInput(recipient, text) {
   });
 }
 
+function getTemplatedMessageInput(recipient, movie, seats) {
+  return JSON.stringify({
+    "messaging_product": "whatsapp",
+    "to": recipient,
+    "type": "template",
+    "template": {
+      "name": "sample_movie_ticket_confirmation2",
+      "language": {
+        "code": "en_US"
+      },
+      "components": [
+        {
+          "type": "header",
+          "parameters": [
+            {
+              "type": "image",
+              "image": {
+                "link": movie.thumbnail
+              }
+            }
+          ]
+        },
+        {
+          "type": "body",
+          "parameters": [
+            {
+              "type": "text",
+              "text": movie.title
+            },
+            {
+              "type": "date_time",
+              "date_time": {
+                "fallback_value": movie.time
+              }
+            },
+            {
+              "type": "text",
+              "text": movie.venue
+            },
+            {
+              "type": "text",
+              "text": seats
+            }
+          ]
+        }
+      ]
+    }
+  }
+  );
+}
+
 module.exports = {
   sendMessage: sendMessage,
-  getTextMessageInput: getTextMessageInput
+  getTextMessageInput: getTextMessageInput,
+  getTemplatedMessageInput: getTemplatedMessageInput
 };
--- routes/welcome.js
+++ routes/welcome.js
@@ -1,25 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var bodyParser = require('body-parser');
-require('dotenv').config()
+var bodyParser = require("body-parser");
+require("dotenv").config();
 const { sendMessage, getTextMessageInput } = require("../messageHelper");
 
 router.use(bodyParser.json());
 
-router.post('/', function(req, res, next) {
-  var data = getTextMessageInput(process.env.RECIPIENT_WAID, 'Welcome to the Movie Ticket Demo App for Node.js!');
-  
+router.post("/", function (req, res, next) {
+  var data = getTextMessageInput(
+    process.env.RECIPIENT_WAID,
+    "Welcome to the Movie Ticket Demo App for Node.js!"
+  );
+
   sendMessage(data)
     .then(function (response) {
-      res.redirect('/');
-      res.sendStatus(200);
-      return;
+      res.redirect("/catalog");
     })
     .catch(function (error) {
       console.log(error);
-      console.log(error.response.data);
+      if (error.response && error.response.data) {
+        console.log(error.response.data);
+      }
       res.sendStatus(500);
-      return;
     });
 });
 
